6e731b06c6c221190981dc74d6c3973266f2b91c
Marquesxy <mairon.msantos@senacsp.edu.br>
2025-02-20 19:21:42 -0300

    feat: exer C3

---
+++ Exerc 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace Exerc_7
+{
+    internal class Program
+    {
+            /*Faça um algoritmo que o usuário escreva o nome e o preço de seis
+            produtos e assim que terminar de escrever o ultimo produto, mostre o
+            nome e o valor do produto mais barato */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos produtos voce quer cadastrar?");
+            int nmrCadastro = Convert.ToInt16(Console.ReadLine());
+            string produtomenor;
+            double menorPreco;
+            for (int i = 0; i < nmrCadastro; i++)
+            {
+                Console.WriteLine("produto a ser cadastrado");
+                string name_produto = Console.ReadLine()!;
+
+                Console.WriteLine("Valor do produto cadastrado");
+                double preco_produto = Convert.ToDouble(Console.ReadLine());
+
+                produtomenor += name_produto;
+                menorPreco += preco_produto;
+                
+            }
+        }
+    }
+}
+3
